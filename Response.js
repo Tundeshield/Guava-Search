218a62caa589e809ae4870bbe84ccbe8082883a3
Tunde Adepegba <tundeshield@gmail.com>
2021-04-23 11:40:45 +0100

    Search results fetching and design

---
+++ Response.js
@@ -0,0 +1,184 @@
+export default {
+	kind: "customsearch#search",
+	url: {
+		type: "application/json",
+		template:
+			"https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+	},
+	queries: {
+		request: [
+			{
+				title: "Google Custom Search - hao",
+				totalResults: "5",
+				searchTerms: "hao",
+				count: 5,
+				startIndex: 1,
+				inputEncoding: "utf8",
+				outputEncoding: "utf8",
+				safe: "off",
+				cx: "47c38e7e3608ec60c",
+			},
+		],
+	},
+	context: {
+		title: "Google",
+	},
+	searchInformation: {
+		searchTime: 0.521828,
+		formattedSearchTime: "0.52",
+		totalResults: "5",
+		formattedTotalResults: "5",
+	},
+	items: [
+		{
+			kind: "customsearch#result",
+			title: "Hao Yu Dim Sum",
+			htmlTitle: "<b>Hao</b> Yu Dim Sum",
+			link:
+				"https://www.google.com/mymaps/viewer?mid=zb40lxu2g9X0.k9zyjzcaTba0&hl=en",
+			displayLink: "www.google.com",
+			snippet:
+				"Open full screen to view more. Hao Yu Dim Sum. Collapse map legend. Map \ndetails. Copy map. Print map. Zoom to viewport. Embed map. Download KML.",
+			htmlSnippet:
+				"Open full screen to view more. <b>Hao</b> Yu Dim Sum. Collapse map legend. Map <br>\ndetails. Copy map. Print map. Zoom to viewport. Embed map. Download KML.",
+			cacheId: "FRywXO61u8YJ",
+			formattedUrl:
+				"https://www.google.com/mymaps/viewer?mid=zb40lxu2g9X0...hl=en",
+			htmlFormattedUrl:
+				"https://www.google.com/mymaps/viewer?mid=zb40lxu2g9X0...hl=en",
+			pagemap: {
+				cse_thumbnail: [
+					{
+						src:
+							"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT4guP_I-njgIxGj2zmhqwXQY8k886Tad2if2JNZ4MuYLe51h1P_DHT4Ig",
+						width: "204",
+						height: "204",
+					},
+				],
+				website: [
+					{
+						image:
+							"https://www.google.com/maps/d/thumbnail?mid=12tp_QzlfrDT0YNyK6hdYJCJOwH0&hl=en",
+						name: "Hao Yu Dim Sum",
+						description: "Hao Yu Dim Sum",
+						url:
+							"https://www.google.com/maps/d/viewer?mid=12tp_QzlfrDT0YNyK6hdYJCJOwH0&hl=en",
+					},
+				],
+				metatags: [
+					{
+						"og:image":
+							"https://www.google.com/maps/d/thumbnail?mid=12tp_QzlfrDT0YNyK6hdYJCJOwH0&hl=en",
+						"og:type": "website",
+						"twitter:card": "summary_large_image",
+						"twitter:title": "Hao Yu Dim Sum",
+						viewport:
+							"initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0,width=device-width",
+						"twitter:description": "Hao Yu Dim Sum",
+						"og:title": "Hao Yu Dim Sum",
+						"og:url":
+							"https://www.google.com/maps/d/viewer?mid=12tp_QzlfrDT0YNyK6hdYJCJOwH0&hl=en",
+						"og:description": "Hao Yu Dim Sum",
+						"twitter:image:src":
+							"https://www.google.com/maps/d/thumbnail?mid=12tp_QzlfrDT0YNyK6hdYJCJOwH0&hl=en",
+					},
+				],
+				cse_image: [
+					{
+						src:
+							"https://khms0.googleapis.com/kh?v=713&hl=en&&x=6458&y=4060&z=13",
+					},
+				],
+			},
+		},
+		{
+			kind: "customsearch#result",
+			title: "Haoma Mining NL: ASX:HAO historical prices - Google Finance",
+			htmlTitle:
+				"Haoma Mining NL: ASX:<b>HAO</b> historical prices - Google Finance",
+			link:
+				"http://www.google.com/finance/historical?q=ASX:HAO&ei=dc_eWLCrFoSf2AbpsaDYAQ",
+			displayLink: "www.google.com",
+			snippet:
+				"Review and download historical prices and charts for Haoma Mining NL(ASX:\nHAO).",
+			htmlSnippet:
+				"Review and download historical prices and charts for Haoma Mining NL(ASX:<br>\n<b>HAO</b>).",
+			cacheId: "bbxjF4tdAwwJ",
+			formattedUrl: "www.google.com/finance/historical?q=ASX:HAO&ei=dc...",
+			htmlFormattedUrl:
+				"www.google.com/finance/historical?q=ASX:<b>HAO</b>&amp;ei=dc...",
+		},
+		{
+			kind: "customsearch#result",
+			title: "Google",
+			htmlTitle: "Google",
+			link: "https://www.google.com/?hl=ms",
+			displayLink: "www.google.com",
+			snippet:
+				"LAPORKAN ISTILAH INI. BATAL. OK. PADAM. Tetapan · Tetapan carian · Carian \nterperinci · Data anda dalam Carian · Sejarah carian · Bantuan carian. Maklum ...",
+			htmlSnippet:
+				"LAPORKAN ISTILAH INI. BATAL. OK. PADAM. Tetapan &middot; Tetapan carian &middot; Carian <br>\nterperinci &middot; Data anda dalam Carian &middot; Sejarah carian &middot; Bantuan carian. Maklum&nbsp;...",
+			cacheId: "BmJvlGDj2sQJ",
+			formattedUrl: "https://www.google.com/?hl=ms",
+			htmlFormattedUrl: "https://www.google.com/?hl=ms",
+			pagemap: {
+				metatags: [
+					{
+						referrer: "origin",
+						viewport: "width=device-width,minimum-scale=1.0",
+						google: "notranslate",
+						"format-detection": "telephone=no",
+					},
+				],
+			},
+		},
+		{
+			kind: "customsearch#result",
+			title: "Google",
+			htmlTitle: "Google",
+			link: "https://www.google.com/?hl=zh_TW",
+			displayLink: "www.google.com",
+			snippet:
+				"透過app 更好搜尋. 在Android 上使用Google app 搜尋更加便利，立即試用？ 不用\n了，謝謝. 好. 首頁 · 搜尋記錄 · 珍藏內容 · 離線搜尋記錄 · 設定 · 你在Google 搜尋 ...",
+			htmlSnippet:
+				"透過app 更好搜尋. 在Android 上使用Google app 搜尋更加便利，立即試用？ 不用<br>\n了，謝謝. 好. 首頁 &middot; 搜尋記錄 &middot; 珍藏內容 &middot; 離線搜尋記錄 &middot; 設定 &middot; 你在Google 搜尋&nbsp;...",
+			cacheId: "df_bsSLygWUJ",
+			formattedUrl: "https://www.google.com/?hl=zh_TW",
+			htmlFormattedUrl: "https://www.google.com/?hl=zh_TW",
+			pagemap: {
+				metatags: [
+					{
+						referrer: "origin",
+						viewport: "width=device-width,minimum-scale=1.0",
+						google: "notranslate",
+						"format-detection": "telephone=no",
+					},
+				],
+			},
+		},
+		{
+			kind: "customsearch#result",
+			title: "Google",
+			htmlTitle: "Google",
+			link: "https://www.google.com/?hl=Fr",
+			displayLink: "www.google.com",
+			snippet:
+				"Avec l'appli, c'est encore mieux. Pour un accès rapide à Google sur Android, \nessayez l'appli. NON MERCI. OUI. Accueil · Historique des recherches ...",
+			htmlSnippet:
+				"Avec l&#39;appli, c&#39;est encore mieux. Pour un accès rapide à Google sur Android, <br>\nessayez l&#39;appli. NON MERCI. OUI. Accueil &middot; Historique des recherches&nbsp;...",
+			cacheId: "e1_QkSxMWHcJ",
+			formattedUrl: "https://www.google.com/?hl=Fr",
+			htmlFormattedUrl: "https://www.google.com/?hl=Fr",
+			pagemap: {
+				metatags: [
+					{
+						referrer: "origin",
+						viewport: "width=device-width,minimum-scale=1.0",
+						google: "notranslate",
+						"format-detection": "telephone=no",
+					},
+				],
+			},
+		},
+	],
+};
---
+++ components/SearchResults.js
@@ -0,0 +1,25 @@
+function SearchResults({ results }) {
+	return (
+		<div className="mx-auto w-full px-3 sm:pl-[14%] lg:pl-60">
+			<p className="text-gray-600 text-md mb-5">
+				About {results.searchInformation?.formattedTotalResults} results(
+				{results.searchInformation?.formattedSearchTime} seconds)
+			</p>
+			{results.items?.map((result) => (
+				<div key={result.link} className="max-w-xl mb-8">
+					<div className="group">
+						<a href={result.link} className="text-sml">
+							{result.formattedUrl}
+						</a>
+						<a href={result.link} className="">
+							<h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline">{result.title}</h2>
+						</a>
+					</div>
+					<p>{result.snippet}</p>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default SearchResults;
